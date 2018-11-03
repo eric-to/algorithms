515d90b8c73f99acb1a0f24d9f36f3efeea86063
Eric To <erto@berkeley.edu>
2018-11-03 02:06:03 -0700

    Add detailed problem description

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,64 @@
 =begin
----------
-Analysis:
----------
+
+---------------
+Analysis
+---------------
 Runtime: O(n^2)
 Space: O(1)
 Best-case: n
----------
+
+
+---------------
 Description
----------
+---------------
 Here I've defined two bubble_sort methods. Both are almost functionally the same, except one
 requires fewer iterations.
 
 Bubble sort is practically slow on almost all real data sets, so it's better to use other
 sorting algorithms. Although, it runs fairly quickly on small arrays and partially sorted
 data (where only a handful of elements are out of place and need to be swapped).
----------
+
+
+---------------
+How it works
+---------------
+  - check every element against every other element in the array, swapping only when 2
+  elements are out of order (accomplished using 2 nested loops)
+
+  - minor optimization: use a boolean to keep track of whether a swap was made on each
+  iteration - if not, then return since the array is already sorted
+
+  - minor optimization (used in better_bubble_sort): instead of iterating through the
+  entire array in the inner loop, only look at n - i - 1 elements, where i is equal to
+  the number of outer iterations we've completed
+
+  why this works: on each iteration of the inner loop, the maximum element gets bubbled up
+  (or swapped) to the end of the array, so after n iterations of the outer loop, the n
+  largest elements in the array are located at the very end - therefore, we can avoid
+  looking at them
+
+  FIRST ITERATION OF INNER LOOP
+  [4, 3, 2, 1]
+   ^
+  [3, 4, 2, 1]
+      ^
+  [3, 2, 4, 1]
+         ^
+  [3, 2, 1, 4] the maximum element gets moved to the end of the array
+            ^
+
+  SECOND ITERATION OF INNER LOOP
+  [3, 2, 1, 4]
+   ^
+  [2, 3, 1, 4]
+      ^
+  [2, 1, 3, 4] note that we don't need to look at 4 since it's in the right position already
+         ^
+  .
+  .
+  .
+  [1, 2, 3, 4] we're done, yay!
+
 =end
 
 def swap(arr, idx_1, idx_2)
