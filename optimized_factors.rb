7e6c199cba21001f62d4479d27562186e44b6877
Eric To <erto@berkeley.edu>
2018-11-04 19:41:28 -0800

    Add optimized_factors

---
+++ optimized_factors.rb
@@ -0,0 +1,33 @@
+require 'benchmark'
+
+def naive_factors(n)
+  list_factors = []
+  (1..n).each { |x| list_factors << x if n % x == 0 }
+  list_factors
+end
+
+def optimized_factors(n)
+  list_factors = []
+  (1..Math.sqrt(n)).each do |x|
+    if n % x == 0
+      list_factors << x
+      list_factors << n / x unless x == n / x
+    end
+  end
+  list_factors
+end
+
+# TEST CODE
+Benchmark.bm(17) do |bm|
+  bm.report("naive_factors") { naive_factors(1_000_000_000) } # finished 84.6587 seconds
+  bm.report("optimized_factors") { optimized_factors(1_000_000_000) } # finished 0.0077 seconds
+end
+
+[1] << 2
+[1].concat([2])
+[1] += [2]
+[1].push(2)
+[1].shift(2)
+[1].unshift(2)
+[1].drop(2)
+[1].take(2)
