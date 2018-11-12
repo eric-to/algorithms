fe06586c7315165515d56c192fa8d02718012ea2
Eric To <erto@berkeley.edu>
2018-11-11 22:17:35 -0800

    Add merge_sort helper

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,5 +1,6 @@
 def merge_sort(arr, &prc)
   return arr if arr.count <= 1
+  prc ||= Proc.new { |x, y| x <=> y }
   mid = arr.count / 2
   left_arr = arr[0...mid]
   right_arr = arr[mid..-1]
@@ -7,5 +8,12 @@ def merge_sort(arr, &prc)
 end
 
 def merge_helper(left, right, &prc)
-  
+  combined_arr = []
+  until left.empty? || right.empty?
+    if prc.call(left.first, right.first) == 1
+      combined_arr << right.shift
+    else
+      combined_arr << left.shift
+    end
+  combined_arr + left + right
 end
